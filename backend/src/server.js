0e82eecd76d8effe70cdd716c0e811e1516a6b6a
limbubijaya <limbubj@ymail.com>
2024-11-27 20:36:45 +0800

    final update

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,9 +20,7 @@ app.use("/api/message", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../../frontend/build", "build", "index.html")
-    );
+    res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
   });
 }
 
